99efb288ea84d0f2ad63c793f64905b59ad4d691
Charlotte Lanuel <lanuel1u@fst-ats-303-l02.fst.site.univ-lorraine.fr>
2018-11-13 11:20:55 +0100

    creation des classes Medieval et Contemporain
    à compléter correctement avec les textures etc

---
+++ src/factory/Contemporain.java
@@ -0,0 +1,16 @@
+package factory;
+
+import com.sun.prism.Texture;
+import model.Bateau;
+
+import java.util.List;
+
+public class Contemporain extends SingletonEpoque {
+
+    //tout est laissé à null pour ne pas avoir d'erreur
+    //sera modifié prochainement
+
+    public Texture getTexture(Bateau b){return null;}
+    public List<Bateau> getFleet(){return null;}
+}
+
---
+++ src/factory/Medieval.java
@@ -0,0 +1,21 @@
+package factory;
+
+import com.sun.prism.Texture;
+import model.Bateau;
+
+import java.util.List;
+
+public class Medieval extends SingletonEpoque {
+
+    //tout est laissé à null pour ne pas avoir d'erreur
+    //sera modifié prochainement
+
+    public Texture getTexture(Bateau b){
+        return null;
+    }
+    public List<Bateau> getFleet(){
+        return null;
+    }
+
+
+}
